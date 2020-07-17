26ebc255b2a4db7bd6db93138ef44c4aeb8773c8
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-17 11:23:45 -0400

    Add AmusementPark model

---
+++ spec/models/ride_spec.rb
@@ -0,0 +1,13 @@
+# require 'rails_helper'
+#
+# RSpec.describe Ride, type: :model do
+#   describe "validations" do
+#     it {should validate_presence_of :name}
+#     it {should validate_presence_of :thrill_rating}
+#   end
+#
+#   describe "relationships" do
+#     it {should belong_to :amusement_parks}
+#     it {should have_many :mechanics}
+#   end
+# end
