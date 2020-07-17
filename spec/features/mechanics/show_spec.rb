f861864d2defdd83da893a48954fd11aa14f807d
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-17 12:54:05 -0400

    Add expectations to mechanic show page spec

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -1,21 +1,36 @@
-# Story 3
-# As a user,
-# When I go to a mechanics show page
-# I see their name, years of experience, and
-# names of all rides they’re working on
-# And I also see a form to add a ride to
-# their workload
-# When I fill in that field with an id
-# of an existing ride and hit submit
-# I’m taken back to that mechanics show page
-# And I see the name of that newly added
-# ride on this mechanics show page
-# Ex:
-# Mechanic: Kara Smith
-# Years of Experience: 11
-# Current rides they’re working on:
-# The Frog Hopper
-# Fahrenheit
-# The Kiss Raise
-# Add a ride to workload:
-# _pretend_this_is_a_textfield_
+require 'rails_helper'
+
+RSpec.describe "mechanic show page", type: :feature do
+   it 'can display information about a given mechanic' do
+    elitch_gardens = AmusementPark.create(name: "Elitch Gardens", admission_price: 65)
+    six_flags = AmusementPark.create(name: "Six Flags New England", admission_price: 50)
+
+    boomerang = elitch_gardens.rides.create(name: "Boomerang", thrill_rating: 8)
+    half_pipe = elitch_gardens.rides.create(name: "Half Pipe", thrill_rating: 7)
+
+    cyborg = six_flags.rides.create(name: "CYBORG Hyper Drive", thrill_rating: 9)
+    thunderbolt = six_flags.rides.create(name: "Thunderbolt", thrill_rating: 5)
+
+    ayano = Mechanic.create(name: "Ayano Okamoto", years_of_experience: 18)
+
+    RideMechanic.create(ride_id: half_pipe.id, mechanic_id: ayano.id)
+    RideMechanic.create(ride_id: boomerang.id, mechanic_id: ayano.id)
+
+    visit "/mechanics/#{ayano.id}"
+
+    expect(page).to have_content("Mechanic: #{ayano.name}")
+    expect(page).to have_content("Years of Experience: #{ayano.years_of_experience}")
+    expect(page).to have_content("Current rides they’re working on:")
+    expect(page).to have_content("#{boomerang.name}")
+    expect(page).to have_content("#{half_pipe.name}")
+
+    expect(page).to have_content("Add a ride to workload:")
+
+    fill_in "Ride ID", with: cyborg.id
+
+    click_on "Submit Ride"
+
+    expect(current_path).to eq("/mechanics/#{ayano.id}")
+    xpect(page).to have_content("#{cyborg.name}")
+  end
+end
